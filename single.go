4ef257241368df0d3762de5effabf8b52dc737ff
Cesar Gimenes <crg@crg.eti.br>
2018-12-16 19:48:03 -0200

    add error return

--- single.go
+++ single.go
@@ -3,17 +3,20 @@ package single
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 func Start(name string) (err error) {
-	f, err := os.OpenFile(name, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0600)
+	f, err := os.OpenFile(filepath.Join(os.TempDir(), name), os.O_RDWR|os.O_CREATE|os.O_EXCL, 0600)
 	if err != nil {
 		return
 	}
 	fmt.Fprintf(f, "%10d", os.Getpid())
-	f.Close()
+	err = f.Close()
+	return
 }
 
-func Stop(name string) {
-	os.Remove(name)
+func Stop(name string) (err error) {
+	err = os.Remove(filepath.Join(os.TempDir(), name))
+	return
 }
