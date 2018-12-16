33395ab12856385e386eeb8deae4e2cae20a45ca
Cesar Gimenes <crg@crg.eti.br>
2018-12-16 20:19:41 -0200

    add comments

--- single.go
+++ single.go
@@ -6,16 +6,22 @@ import (
 	"path/filepath"
 )
 
+// Start checks to see if a process lock file exists,
+// if it does, it returns an error, otherwise, it creates the file.
 func Start(name string) (err error) {
 	f, err := os.OpenFile(filepath.Join(os.TempDir(), name), os.O_RDWR|os.O_CREATE|os.O_EXCL, 0600)
 	if err != nil {
 		return
 	}
-	fmt.Fprintf(f, "%10d", os.Getpid())
+	_, err = fmt.Fprintf(f, "%10d", os.Getpid())
+	if err != nil {
+		return
+	}
 	err = f.Close()
 	return
 }
 
+// Stop remove the lock file
 func Stop(name string) (err error) {
 	err = os.Remove(filepath.Join(os.TempDir(), name))
 	return
